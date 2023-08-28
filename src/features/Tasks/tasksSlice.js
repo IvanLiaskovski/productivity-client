7f70119a7653425450a3c24e14331929584456e6
Ivan Liaskovski <iliaskovski@gmail.com>
2023-08-28 22:04:34 +0200

    Tasks Redux Slice
    - Add normalized state shape
    - Add reducers: create, update, remove, mode, and onlyActive
    - Create basic and memoized selectors

--- src/features/Tasks/tasksSlice.js
+++ src/features/Tasks/tasksSlice.js
@@ -1,20 +1,101 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {
+  createSlice,
+  createEntityAdapter,
+  createSelector,
+} from "@reduxjs/toolkit";
+import { v4 as uuid } from "uuid";
+import { sortTasks } from "./helpers/tasksHelpers";
+import { INITAIAL_TASKS } from "../../data/initialTasksData";
+import { formatDate } from "../../helpers/formatDate";
 
-const initialState = [
-  { id: "1", title: "First Post!", content: "Hello!" },
-  { id: "2", title: "Second Post", content: "More text" },
-];
+const tasksAdapter = createEntityAdapter({});
+const initialTasks = localStorage.getItem("productivityTasks")?.length
+  ? JSON.parse(localStorage.getItem("productivityTasks"))
+  : INITAIAL_TASKS;
 
-const postsSlice = createSlice({
+const initialState = tasksAdapter.getInitialState({
+  ...initialTasks,
+  mode: "day",
+  onlyActive: false,
+});
+
+const tasksSlice = createSlice({
   name: "tasks",
   initialState,
   reducers: {
-    addTask: (state, action) => {
-      state.push(action.payload);
+    createTask: {
+      reducer: (state, action) => {
+        const { date } = action.payload;
+        const mode = state.mode;
+
+        tasksAdapter.addOne(state, {
+          ...action.payload,
+          date: mode === "year" ? new Date(date).getFullYear() : date,
+          type: mode,
+        });
+      },
+      prepare: (data) => ({
+        payload: {
+          ...data,
+          id: uuid(),
+          isCompleted: false,
+          date: formatDate(data.date),
+        },
+      }),
     },
+    updateTask: tasksAdapter.upsertOne,
+    removeTask: tasksAdapter.removeOne,
+    setTasksMode: (state, action) => ({ ...state, mode: action.payload }),
+    setTasksOnlyActive: (state, action) => ({
+      ...state,
+      onlyActive: action.payload,
+    }),
   },
 });
 
-export const { addTask } = postsSlice.actions;
+//Selectors
+export const selectTasks = (state) => state.tasks;
+export const selectTasksMode = (state) => state.tasks.mode;
+export const selectTasksOnlyActive = (state) => state.tasks.onlyActive;
 
-export default postsSlice.reducer;
+export const selectTaskIdsByDate = createSelector(
+  [selectTasks, (_, date) => date],
+  (tasks, date) => {
+    let taskArray = Object.values(tasks.entities).filter(
+      (task) => task?.date === date,
+    );
+
+    if (tasks.onlyActive) {
+      taskArray = taskArray.filter((task) => !task.isCompleted);
+    }
+
+    return sortTasks(taskArray).map((task) => task.id);
+  },
+);
+
+export const selectTasksByDate = createSelector(
+  [selectTasks, (_, date) => date],
+  (tasks, date) => {
+    return sortTasks(
+      Object.values(tasks.entities).filter((task) => task?.date === date),
+    );
+  },
+);
+
+export const {
+  selectAll: selectAllTasks,
+  selectIds: selectAllTaskIds,
+  selectById: selectTaskById,
+  selectEntities,
+} = tasksAdapter.getSelectors(selectTasks);
+
+export const {
+  createTask,
+  updateTask,
+  removeTask,
+  setTasksMode,
+  setTasksOnlyActive,
+  setTaskDate,
+} = tasksSlice.actions;
+
+export default tasksSlice.reducer;
