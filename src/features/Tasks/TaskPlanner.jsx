7ef6f60f175e597132df7efb1a8d3813053e8bc3
Ivan Liaskovski <iliaskovski@gmail.com>
2023-08-28 21:59:09 +0200

    Rename, Remove, and Transfer Files
    - Rename TaskPlanner to Tasks
    - Rename Header to TasksHeader
    - Rename TaskDayView to TasksDayView
    - Rename test for TasksDayView
    - Remove index.css
    - Rename Tasks in the page folder to TasksModule

--- src/features/Tasks/TaskPlanner.jsx
+++
@@ -1,8 +0,0 @@
-import Header from "./components/Header/Header";
-import TaskDay from "./components/TaskDayView/TaskDayView";
-
-function TasksModule() {
-  return <Header />;
-}
-
-export default TasksModule;
