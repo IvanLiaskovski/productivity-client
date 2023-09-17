5656db0136b053538c43f5fdae145077f69f3ad9
Ivan Liaskovski <iliaskovski@gmail.com>
2023-09-17 13:59:06 +0200

    Add Tasks Router

--- src/features/Tasks/Tasks.jsx
+++ src/features/Tasks/Tasks.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { TasksDateRangeProvider } from "./context/TasksDateRangeContext";
-import TasksDayView from "./components/TasksDayView/TasksDayView";
+import { Routes, Route } from "react-router";
+import { Navigate } from "react-router";
 import TasksHeader from "./components/TasksHeader/TasksHeader";
+import TasksNavigation from "./components/TasksNavigation/TasksNavigation";
+import TasksDayView from "./components/TasksDayView/TasksDayView";
 
 function Tasks() {
   const [date, setDate] = useState(new Date());
@@ -11,7 +14,11 @@ function Tasks() {
       <div className="md:w-[100vw - 128px] max-h-screen md:ml-[128px] md:pb-7">
         <div className="container mx-auto max-w-6xl px-5 pt-14 md:flex md:min-h-screen md:flex-col">
           <TasksHeader />
-          <TasksDayView />
+          <TasksNavigation />
+          <Routes>
+            <Route path="/day" element={<TasksDayView />} />
+            <Route index element={<Navigate to="/task/day" replace />} />
+          </Routes>
         </div>
       </div>
     </TasksDateRangeProvider>
