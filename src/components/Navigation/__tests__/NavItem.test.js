77c5fca5bffcd7c537a66597caeb471892a0de7e
Ivan Liaskovski <iliaskovski@gmail.com>
2023-09-17 13:56:09 +0200

    Edit Global Navigation
    - Add React-router Links
    - Move Navigation items data into an object
    - Edit NavItem test (Add MemoryRouter to it)

--- src/components/Navigation/__tests__/NavItem.test.js
+++ src/components/Navigation/__tests__/NavItem.test.js
@@ -1,14 +1,29 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import NavItem from "../NavItem";
 
-test("NavItem active and extended classes tests", () => {
-  render(
-    <NavItem isActive className="mt-2">
-      Dashboard
-    </NavItem>,
+test("NavItem active and extended classes tests", async () => {
+  const wrapper = ({ children }) => (
+    <MemoryRouter>
+      <Routes>
+        <Route path="/task" element={<div>Task</div>} />
+      </Routes>
+      {children}
+    </MemoryRouter>
   );
 
-  const navItem = screen.getByRole("link", { name: "Dashboard" });
+  render(
+    <NavItem url="/task" title="task" className="mt-2">
+      Task
+    </NavItem>,
+    { wrapper },
+  );
+
+  const navItem = screen.getByRole("link", { name: "Task" });
   expect(navItem).toBeInTheDocument();
-  expect(navItem).toHaveClass("border-white", "mt-2");
+  expect(navItem).toHaveClass("border-transparent", "mt-2");
+
+  await userEvent.click(navItem);
+  expect(navItem).toHaveClass("border-white");
 });
