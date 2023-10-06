64b7250b6779bd209802dda1d4bbdc504fabc103
Ivan Liaskovski <iliaskovski@gmail.com>
2023-10-06 21:46:21 +0200

    useCheckTasksURL Hook && Tasks Content Limit
    - Add the useCheckTasksURL hook to validate the task route path
    - Implement tasks content limit based on view and device size

--- src/features/Tasks/components/Buttons/DeleteTaskBtn.jsx
+++ src/features/Tasks/components/Buttons/DeleteTaskBtn.jsx
@@ -1,12 +1,20 @@
 import PropTypes from "prop-types";
+import useTaskComplete from "../../hooks/useTaskComplete";
 import { useSpring, animated } from "react-spring";
 import useTaskDelete from "../../hooks/useTaskDelete";
+import { twMerge } from "tailwind-merge";
 
 import TaskActionBtn from "./TaskActionBtn";
 import { RxCross2 } from "react-icons/rx";
 
 const DeleteTaskBtn = ({ taskId, startAction, isActionStart }) => {
   const deleteTask = useTaskDelete(taskId);
+  const isWeek = useTaskComplete("week");
+
+  const styles = twMerge(
+    "relative -z-10 rounded-full bg-red-300 transition-colors duration-150 hover:bg-red-400",
+    isWeek ? "h-4 w-4" : "h-6 w-6",
+  );
 
   const [springStyles, springApi] = useSpring(() => ({
     from: { transform: "scale(1)" },
@@ -24,7 +32,7 @@ const DeleteTaskBtn = ({ taskId, startAction, isActionStart }) => {
 
   return (
     <animated.div
-      className="relative -z-10 h-6 w-6 rounded-full bg-red-300 transition-colors duration-150 hover:bg-red-400"
+      className={styles}
       style={springStyles}
       data-testid="animation-wrapper"
     >
--- src/features/Tasks/components/TaskItem/TaskContent.jsx
+++ src/features/Tasks/components/TaskItem/TaskContent.jsx
@@ -1,12 +1,30 @@
 import PropTypes from "prop-types";
+import { twMerge } from "tailwind-merge";
+import useCheckTasksURL from "../../hooks/useCheckTasksURL";
+import { useMediaQuery } from "react-responsive";
 import AppearAnimation from "../../../../components/AnimationsHOC/AppearAnimation";
 
 const TaskContent = ({ content = "..." }) => {
+  const isWeek = useCheckTasksURL("week");
+  const isScreenLarge = useMediaQuery({ query: "(min-width: 1024px)" });
+
+  const styles = twMerge(
+    "relative z-20 break-words whitespace-break-spaces bg-transparent font-sans",
+    isWeek ? "px-3 xl:px-4 md:py-1 xl:py-2 md:text-xs xl:text-sm" : "px-8 py-2",
+  );
+
+  const contentValue =
+    isScreenLarge && isWeek && content.length > 52
+      ? `${String(content).slice(0, 52)}...`
+      : !isScreenLarge && isWeek && content.length > 12
+      ? `${String(content).slice(0, 12)}...`
+      : content.length > 172
+      ? `${String(content).slice(0, 172)}...`
+      : content;
+
   return (
     <AppearAnimation className="relative z-10" animationType="slideDown" reset>
-      <pre className="relative z-20 break-words bg-transparent font-sans">
-        {content}
-      </pre>
+      <pre className={styles}>{contentValue}</pre>
     </AppearAnimation>
   );
 };
