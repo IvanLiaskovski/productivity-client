8e6a5d6299c4fe32ef581a0decc463f059de7856
Ivan Liaskovski <iliaskovski@gmail.com>
2023-10-19 19:39:55 +0200

    TasksManualDateChangeContext
    - Introduce TasksManualDateChangeContext for More Precise Task Date Range Adjustments

--- src/features/Tasks/Tasks.jsx
+++ src/features/Tasks/Tasks.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { TasksDateProvider } from "./context/TasksDateContext";
 import { TasksDatesRangeProvider } from "./context/TasksDatesRangeContext";
+import { TasksManualDateChangeProvider } from "./context/TasksManualDateChangeContext";
 import { Routes, Route, Navigate, useLocation } from "react-router";
 import TasksHeader from "./components/TasksHeader/TasksHeader";
 import TasksNavigation from "./components/TasksNavigation/TasksNavigation";
@@ -10,6 +11,7 @@ import moment from "moment";
 import { useMediaQuery } from "react-responsive";
 
 function Tasks() {
+  const [isManualChange, setManualChange] = useState(false);
   const [taskDate, setTaskDate] = useState(moment().format("YYYY-MM-DD"));
   const [date, setDate] = useState(new Date(moment().format("YYYY-MM-DD")));
   const isMediumScreen = useMediaQuery({ query: "(min-width: 768px)" });
@@ -20,30 +22,35 @@ function Tasks() {
   }, [location]);
 
   return (
-    <TasksDatesRangeProvider datesRange={date} setDatesRange={setDate}>
-      <TasksDateProvider date={taskDate} setDate={setTaskDate}>
-        <div className="md:w-[100vw - 128px] max-h-screen md:ml-[128px] md:pb-7">
-          <div className="container mx-auto px-4 pt-14 md:flex md:min-h-screen md:max-w-[80vw] md:flex-col md:px-5">
-            <TasksHeader />
-            {isMediumScreen && <TasksNavigation />}
-            <Routes>
-              <Route path="/day" element={<TasksDayView />} />
-              <Route
-                path="/week"
-                element={
-                  isMediumScreen ? (
-                    <TasksWeekView />
-                  ) : (
-                    <Navigate to="/task/day" />
-                  )
-                }
-              />
-              <Route index element={<Navigate to="/task/day" replace />} />
-            </Routes>
+    <TasksManualDateChangeProvider
+      isManualChange={isManualChange}
+      setManualChange={setManualChange}
+    >
+      <TasksDatesRangeProvider datesRange={date} setDatesRange={setDate}>
+        <TasksDateProvider date={taskDate} setDate={setTaskDate}>
+          <div className="md:w-[100vw - 128px] md:ml-[128px] md:pb-7">
+            <div className="container mx-auto px-4 pt-14 md:flex md:min-h-screen md:max-w-[80vw] md:flex-col md:px-5">
+              <TasksHeader />
+              {isMediumScreen && <TasksNavigation />}
+              <Routes>
+                <Route path="/day" element={<TasksDayView />} />
+                <Route
+                  path="/week"
+                  element={
+                    isMediumScreen ? (
+                      <TasksWeekView />
+                    ) : (
+                      <Navigate to="/task/day" />
+                    )
+                  }
+                />
+                <Route index element={<Navigate to="/task/day" replace />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </TasksDateProvider>
-    </TasksDatesRangeProvider>
+        </TasksDateProvider>
+      </TasksDatesRangeProvider>
+    </TasksManualDateChangeProvider>
   );
 }
 
