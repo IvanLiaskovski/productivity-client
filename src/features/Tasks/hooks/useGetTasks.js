575415acae65d23382df68b42b0151a8f054705c
Ivan Liaskovski <iliaskovski@gmail.com>
2023-10-25 20:43:32 +0200

    Extend useGetTasks hook and selectTasksByDate
    - Extended the 'useGetTasks' hooks to include additional
      functionality within the type argument.
    
    - Enhanced the 'selectTasksByDate' selector to accommodate the type argument,
      providing more flexibility in task retrieval.

--- src/features/Tasks/hooks/useGetTasks.js
+++ src/features/Tasks/hooks/useGetTasks.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { selectTasksByDate, selectTasksMode } from "../tasksSlice";
+import { selectTasksByDate } from "../tasksSlice";
 import useCheckTasksURL from "./useCheckTasksURL";
 import {
   createWeekDatesRange,
@@ -8,20 +8,22 @@ import {
 import moment from "moment";
 
 export function useGetTasks(date) {
-  const mode = useSelector(selectTasksMode);
   const isWeek = useCheckTasksURL("week");
   const isMonth = useCheckTasksURL("month");
+  const isYear = useCheckTasksURL("year");
 
   if (isMonth) {
     date = createMonthDatesRange(date, true, false);
   } else if (isWeek) {
-    date = createWeekDatesRange(date, true).slice(0, 7);
+    date = createWeekDatesRange(date, true).slice(7, 14);
+  } else if (isYear) {
+    date = moment().format("YYYY");
   } else {
     date = moment(date).format("YYYY-MM-DD");
   }
 
   const tasks = useSelector((state) =>
-    selectTasksByDate(state, mode === "year" ? date.getFullYear() : date),
+    selectTasksByDate(state, date, isYear ? "year" : "day"),
   );
 
   return tasks;
