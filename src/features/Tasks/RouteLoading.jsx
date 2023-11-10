ec3a8470097f3456443035a943b075ef88855782
Ivan Liaskovski <iliaskovski@gmail.com>
2023-11-10 21:17:51 +0100

    Loading Skeletons

---
+++ src/features/Tasks/RouteLoading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const RouteLoading = () => {
+  return (
+    <div className="h-16 w-full animate-pulse rounded-lg bg-block opacity-70 lg:h-44"></div>
+  );
+};
+
+export default RouteLoading;
