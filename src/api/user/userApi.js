f9e11ddaed2be4294b61f4ec40ca4d99c16f900a
Ivan Liaskovski <iliaskovski@gmail.com>
2023-12-10 21:42:27 +0100

    API(RTK) - User LogIn endpoint

--- src/api/user/userApi.js
+++ src/api/user/userApi.js
@@ -32,7 +32,35 @@ const userApi = api.injectEndpoints({
         }
       },
     }),
+    logIn: build.mutation({
+      query: ({ email, password }) => ({
+        body: gql`
+          mutation Mutation($input: LoginUserInput!) {
+            loginUser(input: $input) {
+              token {
+                accessToken
+                expiresIn
+              }
+            }
+          }
+        `,
+        variables: { input: { email, password } },
+      }),
+      async onQueryStarted(_, { queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          console.log(data.loginUser.token);
+          const token = data.loginUser.token.accessToken;
+          const expiredTime = data.loginUser.token.expiresIn;
+          console.log(token);
+          Cookies.set("productivity-token", token, 1); // Temporary set to 1 day
+          window.location = "/";
+        } catch (err) {
+          console.log("SignUp API error", err);
+        }
+      },
+    }),
   }),
 });
 
-export const { useSignUpMutation } = userApi;
+export const { useSignUpMutation, useLogInMutation } = userApi;
