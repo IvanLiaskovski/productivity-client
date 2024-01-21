060ab0078b5f8133cc7a5314a70e6e202e69ee46
Ivan Liaskovski <iliaskovski@gmail.com>
2024-01-21 19:50:24 +0100

    Implemented error popups, added error boundaries to the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,27 @@ import { Routes, Route } from "react-router";
 import Navigation from "./components/Navigation/Navigation";
 import TasksModule from "./pages/TasksModule";
 import UserRoutes from "./features/User/UserRoutes";
+import { ErrorBoundary } from "react-error-boundary";
+import GlobalError from "./components/Errors/GlobalError/GlobalError";
 
 import OpenCreatePanelProvider from "./context/OpenCreatePanelContext";
 import { AuthProvider } from "./context/AuthenticationContext";
 
 function App() {
   return (
-    <AuthProvider>
-      <OpenCreatePanelProvider>
-        <div>
-          <Navigation />
-          <Routes>
-            <Route path="task/*" element={<TasksModule />} />
-            <Route path="user/*" element={<UserRoutes />} />
-          </Routes>
-        </div>
-      </OpenCreatePanelProvider>
-    </AuthProvider>
+    <ErrorBoundary fallbackRender={GlobalError}>
+      <AuthProvider>
+        <OpenCreatePanelProvider>
+          <div>
+            <Navigation />
+            <Routes>
+              <Route path="task/*" element={<TasksModule />} />
+              <Route path="user/*" element={<UserRoutes />} />
+            </Routes>
+          </div>
+        </OpenCreatePanelProvider>
+      </AuthProvider>
+    </ErrorBoundary>
   );
 }
 
