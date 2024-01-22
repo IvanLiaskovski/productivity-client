da465f5df1275f0a80ec5b3cb6a42c29b6627d87
Ivan Liaskovski <iliaskovski@gmail.com>
2024-01-22 21:00:45 +0100

    useTaskDelete - check if user is authenticated

--- src/features/Tasks/hooks/useTaskDelete.js
+++ src/features/Tasks/hooks/useTaskDelete.js
@@ -1,15 +1,25 @@
+import { useCheckAuth } from "../../../context/AuthenticationContext";
 import { useDeleteTaskMutation } from "../../../api/api";
 import { useDispatch } from "react-redux";
 import { removeTask } from "../tasksSlice";
 
 export default function useTaskDelete(id) {
+  const { isDemo, user } = useCheckAuth();
   const [removeTaks] = useDeleteTaskMutation();
   const dispatch = useDispatch();
 
   return function deleteTask() {
-    setTimeout(() => {
-      removeTaks(id);
-      dispatch(removeTask(id));
+    setTimeout(async () => {
+      let isSuccessful = false;
+      if (user) {
+        await removeTaks(id)
+          .unwrap()
+          .then(() => (isSuccessful = true));
+      }
+
+      if (isDemo || isSuccessful) {
+        dispatch(removeTask(id));
+      }
     }, 300);
   };
 }
