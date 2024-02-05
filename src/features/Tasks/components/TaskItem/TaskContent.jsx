e71d7ba076e87617505d9676ca080a3f895aad4c
Ivan Liaskovski <iliaskovski@gmail.com>
2024-02-05 22:36:47 +0100

    Productivity Client, task content
    - Refactoring - limit symbols with css instead of js

--- src/features/Tasks/components/TaskItem/TaskContent.jsx
+++ src/features/Tasks/components/TaskItem/TaskContent.jsx
@@ -2,32 +2,17 @@ import PropTypes from "prop-types";
 import { memo } from "react";
 import { twMerge } from "tailwind-merge";
 import useCheckTasksURL from "../../hooks/useCheckTasksURL";
-import { useMediaQuery } from "react-responsive";
 import AppearAnimation from "../../../../components/AnimationsHOC/AppearAnimation";
 
 const TaskContent = memo(({ taskId, name = "..." }) => {
   const isMonthOrWeek = useCheckTasksURL(["month", "week"]);
-  const isScreenSmall = useMediaQuery({ query: "(max-width: 767px)" });
-  const isScreenMedium = useMediaQuery({ query: "(min-width: 768px)" });
-  const isScreenLarge = useMediaQuery({ query: "(min-width: 1024px)" });
 
   const styles = twMerge(
-    "relative z-20 break-words whitespace-break-spaces bg-transparent font-sans",
+    "box relative z-20 line-clamp-3 text-ellipsis whitespace-break-spaces break-words bg-transparent font-sans overflow-hidden",
     isMonthOrWeek
-      ? "px-3 xl:px-4 md:py-1 xl:py-2 md:text-xs xl:text-base"
+      ? "px-3 xl:px-4 md:py-1  md:text-xs xl:text-base"
       : "px-8 py-2",
   );
-
-  const nameValue =
-    isScreenLarge && isMonthOrWeek && name.length > 52
-      ? `${String(name).slice(0, 52)}...`
-      : !isScreenLarge && isMonthOrWeek && name.length > 12
-      ? `${String(name).slice(0, 12)}...`
-      : name.length > 172
-      ? `${String(name).slice(0, 172)}...`
-      : name;
-
-  const hideTooltip = (name === nameValue && isScreenMedium) || isScreenSmall;
 
   return (
     <AppearAnimation className="relative z-10" animationType="slideDown" reset>
@@ -38,10 +23,10 @@ const TaskContent = memo(({ taskId, name = "..." }) => {
         data-tooltip-place="bottom-end"
         data-tooltip-position-strategy="absolute"
         data-tooltip-float={true}
-        data-tooltip-hidden={hideTooltip}
+        data-tooltip-hidden={!isMonthOrWeek}
         data-tooltip-delay-show={500}
       >
-        {nameValue}
+        {name}
       </pre>
     </AppearAnimation>
   );
